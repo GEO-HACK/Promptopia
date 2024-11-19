d1a90d1ee1d23374c586e1c608eb67d9440a8e09
GEO-HACK <geoffreykirumba0@gmail.com>
2024-11-19 22:09:44 +0300

    added the page for the feed

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,10 +2,13 @@
 
 import { useState } from "react"
 import { useSession } from "next-auth/react"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import Form from '@components/Form'
 
 const CreatePrompt  = () => {
+
+    const router = useRouter();
+    const {data : session} = useSession();
 
     const [submitting, setSubmitting] = useState(false);
     const [post, setPost] = useState({
@@ -28,7 +31,7 @@ const CreatePrompt  = () => {
                 })
             })
             if(res.ok){
-                Router.push('/');
+                router.push('/');
             }
 
 
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+"use client"
+
+import { useState , useEffect} from 'react'
+import PromptCard from './PromptCard'
+
+//a private component only to this file
+const PromptCardList = ({ data, handleTagClick}) => {
+  return (
+    <div className='mt-16 prompt_layout '>
+
+    </div>
+  )
+
+}
+ 
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+   const handleSearchChange = (e) => {
+
+   }
   return (
-    <div>
-      feed
-    </div>
+    <section  className='feed '>
+
+      <form className='relative w-full flex-center'>
+        <input type="text"
+        placeholder='Search for a tag or a username'
+        value={searchText}
+        onChange={handleSearchChange}
+        required
+        className='search_input  peer'
+         />
+         
+
+      </form>
+      <PromptCardList
+          data={[]}
+          handleTagClick= {() => {}}
+
+      />
+
+    </section>
   )
 }
 
