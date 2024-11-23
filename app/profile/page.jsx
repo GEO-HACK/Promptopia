2e732fec01eb6491721c11d04f76b3e7e9beb306
GEO-HACK <geoffreykirumba0@gmail.com>
2024-11-23 17:46:29 +0300

    CRUD

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+"use client"
 
-const  Profile = () => {
+import { useState,useEffect} from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+
+import Profile from '@components/Profile'
+
+const  myProfile = () => {
+
+  const router = useRouter()
+  const {data : session }= useSession();
+  const [posts, setPosts] = useState([])
+  
+     useEffect(() => {
+      const fetchPrompts = async () => {
+        const res = await fetch(`/api/users/${session?.user.id}/posts`);
+        const data = await res.json();
+  
+        setPosts(data);
+  
+      }
+      if (session?.user.id) fetchPrompts();
+     },[])
+
+
+    const handleEdit = async (post) => {
+      router.push(`/update-prompt?id=${post._id}`)
+
+    }
+    const handleDelete =  async(post) => {
+      const hasConfirmed = confirm(" Are you sure you want to  delete this prompt?")
+
+      if(hasConfirmed) {
+        try{
+          await fetch (`/api/prompt/${post._id.toString()}`, {
+            method: 'DELETE'
+          })
+
+          const filteredPosts = posts.filter((p) =>
+          p._id !== post._id)
+          setPosts(filteredPosts)
+        }catch(error){
+          console.log(error)
+        }
+      }
+    }
   return (
-    <div>
-      profile
-    </div>
+    <Profile
+     name= "My"
+     desc = "welcome to your personalized profile page"
+     data = {posts}//fetching personalized data from the api
+     handleEdit={handleEdit}
+     handleDelete={handleDelete}
+     />
   )
 }
 
-export default Profile
+export default myProfile
